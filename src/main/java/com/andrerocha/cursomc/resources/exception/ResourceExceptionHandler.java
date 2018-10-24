eed393d85e58238b1bb0aa1a69a577748174ec0c
André Ribeiro <andreribeiro13@hotmail.com>
2018-10-24 17:30:42 -0300

    deletendo uma categoria

--- src/main/java/com/andrerocha/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/andrerocha/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.andrerocha.cursomc.services.exception.DataIntegrityException;
 import com.andrerocha.cursomc.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,5 +17,11 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		StandarError err = new StandarError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandarError> objectNotFound(DataIntegrityException e, HttpServletRequest request) {
+		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
